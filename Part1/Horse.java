946bd6d4c13d6e7470e8f2bbd3b27e1cc92297d1
Darkfury12 <Sabeehashir10@gmail.com>
2025-04-10 14:30:00 +0100

    fixed the horse class constructor and its methods

---
+++ Part1/Horse.java
@@ -0,0 +1,93 @@
+import javax.swing.ImageIcon;
+
+/**
+ * Write a description of class Horse here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Horse
+{
+     private String name;
+    private char symbol;
+    private int distanceTravelled;
+    private boolean hasFallen;
+    private double confidence;
+    
+
+    
+    
+      
+    //Constructor of class Horse
+    /**
+     * Constructor for objects of class Horse
+     */
+    public Horse(char horseSymbol, String horseName, double horseConfidence)
+    {
+        
+        this.symbol = horseSymbol;
+        this.name = horseName;
+        this.confidence = horseConfidence;
+        this.distanceTravelled = 0;
+        this.hasFallen = false;
+    }
+    
+    
+    
+    //Other methods of class Horse
+    public void fall()
+    {
+        if(hasFallen) {
+            return; // Already fallen, no need to fall again
+        }
+        hasFallen = true;
+        this.setConfidence(Math.max(0.0, this.getConfidence() - 0.05));
+    }
+    
+    public double getConfidence()
+    {
+        return confidence;
+    }
+    
+    public int getDistanceTravelled()
+    {
+        return distanceTravelled;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public char getSymbol()
+    {
+        return symbol;
+    }
+    
+    public void goBackToStart()
+    {
+        distanceTravelled = 0;
+        hasFallen = false;
+    }
+    
+    public boolean hasFallen()
+    {
+        return hasFallen;
+    }
+
+    public void moveForward()
+    {
+        distanceTravelled += (int) (Math.random() * 2 * confidence + 0.5); // Random movement
+    }
+
+    public void setConfidence(double newConfidence)
+    {
+        this.confidence = newConfidence;
+    }
+    
+    public void setSymbol(char newSymbol)
+    {
+        this.symbol = newSymbol;
+    }
+    
+}
